1fdbac606183f28e18f2042a9edb8432dc6405a6
Vanshi Chandra <vanshichandra26@gmail.com>
2025-09-11 16:15:18 +0530

    initial commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css"; // or "./style.css" if that’s what you’re using
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
